dfc119a13a05e11cb1af0bc2759836be69620d3e
vickellar <130662668+vickellar@users.noreply.github.com>
2024-05-20 12:30:59 +0200

    Update parallel programming for pi.c

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
-#include<studio.h>
+⁷// parallel programming for calculatiing pi 
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int n = 10000; // Number of terms in the Leibniz series
+    double pi_estimate = 0.0;
+
+    #pragma omp parallel for reduction(+:pi_estimate)
+    for (int i = 0; i < n; i++) {
+        double term = (i % 2 == 0) ? 1.0 / (2 * i + 1) : -1.0 / (2 * i + 1);
+        pi_estimate += term;
+
+        // Print intermediate value for each thread
+        printf("Thread %d: Partial sum = %.10lf\n", omp_get_thread_num(), pi_estimate);
+    }
+
+    pi_estimate *= 4.0; // Multiply by 4 to get the final estimate of π
+
+    printf("Estimated value of π using Leibniz series: %.10lf\n", pi_estimate);
+
+    return 0;
+}
